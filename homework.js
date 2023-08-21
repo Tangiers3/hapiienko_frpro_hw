279eca354d0564a5be54a471f5738e0a51bcf8ac
Tangiers3 <vadym.hapiienko@gmail.com>
2023-08-20 22:15:22 -0600

    first homework 3_1 done

---
+++ homework.js
@@ -0,0 +1,16 @@
+let firstNumber = Number(prompt("Enter the first number:"));
+
+let secondNumber = Number (prompt("Enter the second number:"));
+
+let addition = firstNumber + secondNumber;
+let subtraction = firstNumber - secondNumber;
+let multiplication = firstNumber * secondNumber;
+let division = firstNumber / secondNumber;
+
+
+let message = firstNumber + " + " + secondNumber + " = " + addition + "\n" +
+    firstNumber + " - " + secondNumber + " = " + subtraction + "\n" +
+    firstNumber + " * " + secondNumber + " = " + multiplication + "\n" +
+    firstNumber + " / " + secondNumber + " = " + division;
+
+alert(message);
